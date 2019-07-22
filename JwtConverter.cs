d931c1d0afe4f3d0100061023a8acc6815f42246
Neander Wendel <neanderlink123@gmail.com>
2019-07-21 22:35:09 -0300

    First commit

---
+++ JwtConverter.cs
@@ -0,0 +1,39 @@
+﻿using JwtToObject.Core.Models;
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+
+namespace JwtToObject.Core
+{
+    public class JwtConverter
+    {
+        public static JwtModel ConvertToModel(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jsonToken = handler.ReadToken(token) as JwtSecurityToken;
+
+            return new JwtModel(
+                    GetClaimValue(jsonToken, "nameid"),
+                    GetClaimValue(jsonToken, "unique_name"),
+                    GetClaimValue(jsonToken, "email"),
+                    GetClaimValue(jsonToken, ClaimTypes.MobilePhone),
+                    GetClaimValue(jsonToken, "role"),
+                    ToDateTimeFromEpoch(jsonToken.Payload.Nbf.Value),
+                    ToDateTimeFromEpoch(jsonToken.Payload.Exp.Value),
+                    ToDateTimeFromEpoch(jsonToken.Payload.Iat.Value)
+                );
+        }
+
+        private static string GetClaimValue(JwtSecurityToken token, string type)
+        {
+            return token.Claims.FirstOrDefault(x => x.Type == type)?.Value;
+        }
+
+        private static DateTime ToDateTimeFromEpoch(long intDate)
+        {
+            var timeInTicks = intDate * TimeSpan.TicksPerSecond;
+            return new DateTime(1970, 1, 1, 0, 0, 0, 0, DateTimeKind.Utc).AddTicks(timeInTicks);
+        }
+    }
+}
---
+++ Models/JwtModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace JwtToObject.Core.Models
+{
+    public class JwtModel
+    {
+        public JwtModel(string nameIdentifier, string name, string email, string mobilePhone, string role, 
+            DateTime notValidBefore, DateTime expiration, DateTime issuedAt)
+        {
+            NameIdentifier = nameIdentifier;
+            Name = name;
+            Email = email;
+            MobilePhone = mobilePhone;
+            Role = role;
+            NotValidBefore = notValidBefore;
+            Expiration = expiration;
+            IssuedAt = issuedAt;
+        }
+
+        public string NameIdentifier { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string MobilePhone { get; set; }
+        public string Role { get; set; }
+        public DateTime NotValidBefore { get; set; }
+        public DateTime Expiration { get; set; }
+        public DateTime IssuedAt { get; set; }
+    }
+}
